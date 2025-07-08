8bb0f51e422294dde9d21125251efd7aec1d1c52
Suman Bej <sumanbej.glt@gmail.com>
2025-07-08 21:59:28 +0530

    first commit

---
+++ src/main/java/com/smartKwikReply/smartKwikReply/SmartKwikReplyApplication.java
@@ -0,0 +1,13 @@
+package com.smartKwikReply.smartKwikReply;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartKwikReplyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartKwikReplyApplication.class, args);
+	}
+
+}
